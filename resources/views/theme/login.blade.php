5af0898e23d89049b1e5cca041d9148481d99617
Mohamed Ali <jrali20394@gmail.com>
2025-02-15 19:10:57 +0200

    initial commit

--- resources/views/theme/login.blade.php
+++ resources/views/theme/login.blade.php
@@ -1,6 +1,6 @@
 @extends('theme.master')
 @section('title', 'Login')
-@
+
 
 
 @section('content')
@@ -12,18 +12,22 @@
     <section class="section-margin--small section-margin">
         <div class="container">
             <div class="row">
-                <div class="col-6 mx-auto">
-                    <form action="#/" class="form-contact contact_form" action="contact_process.php" method="post"
-                        id="contactForm" novalidate="novalidate">
+                <div class="mx-auto col-6">
+                    <form action="{{ route('login') }}" class="form-contact contact_form" action="contact_process.php"
+                        method="post" id="contactForm" novalidate="novalidate">
+                        @csrf
                         <div class="form-group">
-                            <input class="form-control border" name="email" id="email" type="email"
-                                placeholder="Enter email address">
+                            <input class="border form-control" name="email" id="email" type="email"
+                                placeholder="Enter email address" value="{{ old('email') }}">
+                            <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger" />
                         </div>
                         <div class="form-group">
-                            <input class="form-control border" name="password" id="name" type="password"
+                            <input class="border form-control" name="password" id="name" type="password"
                                 placeholder="Enter your password">
+                            <x-input-error :messages="$errors->get('password')" class="mt-2 text-danger" />
                         </div>
-                        <div class="form-group text-center text-md-right mt-3">
+                        <div class="mt-3 text-center form-group text-md-right">
+                            <a href="{{ route('register') }}" class="mx-3"> sign up instead?</a>
                             <button type="submit" class="button button--active button-contactForm">Login</button>
                         </div>
                     </form>
